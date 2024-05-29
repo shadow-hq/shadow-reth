41e916493b4dd2def40130aeed2e85dd4fc10462
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2024-05-29 17:23:31 +0200

    chore(deps): update and clean up dependencies (#12)
    * clean up unused dependencies
    
    * use revm re-exported by reth

--- crates/exex/src/execution.rs
+++ crates/exex/src/execution.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use eyre::Result;
 use reth_evm_ethereum::EthEvmConfig;
 use reth_node_api::{ConfigureEvm, ConfigureEvmEnv};
@@ -5,16 +7,15 @@ use reth_primitives::{
     revm::env::fill_tx_env, Block, BlockWithSenders, ChainSpec, Header, TransactionSigned,
 };
 use reth_provider::StateProvider;
-use reth_tracing::tracing::{debug, error};
-use revm::{
+use reth_revm::{
     db::{states::bundle_state::BundleRetention, State},
     primitives::{
         CfgEnvWithHandlerCfg, EVMError, ExecutionResult, HashMap, ResultAndState, B256, U256,
     },
     DatabaseCommit, Evm, StateBuilder,
 };
+use reth_tracing::tracing::{debug, error};
 use shadow_reth_common::{ShadowLog, ToLowerHex};
-use std::sync::Arc;
 
 use crate::db::ShadowDatabase;
 
